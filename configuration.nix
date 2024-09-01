5a0390b7b195327fe08abdfaf4abd40634ef2c1b
Robert Lancer <robert.lancer@gmail.com>
2024-09-01 15:48:17 -0400

    added nerdfront for starship

--- configuration.nix
+++ configuration.nix
@@ -171,8 +171,7 @@ home-manager.users.rob = { pkgs, ... }: {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+   pkgs.nerdfonts # needed for starship
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
